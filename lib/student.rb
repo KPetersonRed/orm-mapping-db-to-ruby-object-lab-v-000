d4d5a8d49de1b04b5f81ddaa7c363ff4960fdeb5
Kristy Peterson <kpeterson.kap@gmail.com>
2017-11-06 02:30:12 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -111,7 +111,7 @@ def self.first_student_in_grade_10
   end.first
 end
 
-def all_students_in_grade_X(x)
+def self.all_students_in_grade_X(x)
   sql = <<-SQL
     SELECT * FROM students
     WHERE grade = x
